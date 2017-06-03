74d00926c553e68bac7df8c6b17de52c58125e28
kawsheek <kask94@gmail.com>
2017-06-03 00:31:44 -0700

    Initial commit

---
+++ app/src/main/java/com/dev/kaushik/jbcchat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.dev.kaushik.jbcchat;
+
+/**
+ * Created by Kaushik Das on 5/5/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}

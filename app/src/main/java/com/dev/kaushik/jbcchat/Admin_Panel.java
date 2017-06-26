e2f5abd71c02f96d781e11cfd6303f225697a629
kawsheek <kask94@gmail.com>
2017-06-26 09:37:40 +0530

    admin panel done

--- app/src/main/java/com/dev/kaushik/jbcchat/Admin_Panel.java
+++ app/src/main/java/com/dev/kaushik/jbcchat/Admin_Panel.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Admin_Panel extends AppCompatActivity {
@@ -18,6 +20,7 @@ public class Admin_Panel extends AppCompatActivity {
     FirebaseUser user;
     FirebaseAuth mAuth;
     FirebaseDatabase mdatabase;
+    DatabaseReference mRef;
     FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
@@ -25,12 +28,15 @@ public class Admin_Panel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__panel);
 
-
+        mdatabase = FirebaseDatabase.getInstance();
+        mRef = mdatabase.getReference("Lists");
         Button sign_out = (Button)findViewById(R.id.sign_out);
         final EditText addCourses = (EditText)findViewById(R.id.add_courses);
-        EditText addFacultyNames = (EditText)findViewById(R.id.add_faculty_names);
+        final EditText addFacultyNames = (EditText)findViewById(R.id.add_faculty_names);
+        final EditText addFacultyQuals = (EditText)findViewById(R.id.add_faculty_quals);
         Button buttonAddCourses = (Button) findViewById(R.id.btn_add_courses);
         Button buttonAddFacultyNames = (Button)findViewById(R.id.btn_add_faculty_names);
+        final Spinner dept = (Spinner)findViewById(R.id.dept);
 
         mAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -65,6 +71,20 @@ public class Admin_Panel extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String CourseName = addCourses.getText().toString();
+                String DeptName = dept.getSelectedItem().toString();
+                mRef.child(DeptName).child("CourseList").child(CourseName).setValue("null");
+                addCourses.setText("");
+
+            }
+        });
+        buttonAddFacultyNames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String FacultyName = addFacultyNames.getText().toString();
+                String DeptName = dept.getSelectedItem().toString();
+                String FacultyQuals = addFacultyQuals.getText().toString();
+                mRef.child(DeptName).child("FacultyList").child(FacultyName).setValue(FacultyQuals);
+                addFacultyNames.setText("");
             }
         });
     }
